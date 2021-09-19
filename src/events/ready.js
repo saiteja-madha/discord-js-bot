46801db14e323708cdc3da6c280478a332a2186e
Sai Teja Madha <42540377+saiteja-madha@users.noreply.github.com>
2021-09-19 19:33:38 +0530

    bot presence

--- src/events/ready.js
+++ src/events/ready.js
@@ -9,6 +9,10 @@ const { getSettings } = require("@schemas/guild-schema");
 module.exports = async (client) => {
   console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
 
+  // Update Bot Presence
+  updatePresence(client);
+  setInterval(() => updatePresence(client), 10 * 60 * 1000);
+
   // Register Interactions
   if (client.config.INTERACTIONS.GLOBAL) await client.registerInteractions();
   else await client.registerInteractions(client.config.INTERACTIONS.TEST_GUILD_ID);
@@ -26,5 +30,23 @@ module.exports = async (client) => {
   client.guilds.cache.forEach(async (guild) => {
     const settings = await getSettings(guild);
     if (settings.invite.tracking) inviteHandler.cacheGuildInvites(guild);
+  });
+};
+
+/**
+ * @param {BotClient} client
+ */
+const updatePresence = (client) => {
+  const guilds = client.guilds.cache;
+  const members = guilds.map((g) => g.memberCount).reduce((partial_sum, a) => partial_sum + a, 0);
+
+  client.user.setPresence({
+    status: "online",
+    activities: [
+      {
+        name: `${members} members in ${guilds.size} servers`,
+        type: "WATCHING",
+      },
+    ],
   });
 };
