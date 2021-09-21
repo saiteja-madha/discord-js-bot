24a832e6a630131ffc2095064476c7eea746aa6b
Sai Teja Madha <42540377+saiteja-madha@users.noreply.github.com>
2021-09-22 00:29:12 +0530

    Update paste.js

--- src/commands/utility/paste.js
+++ src/commands/utility/paste.js
@@ -1,46 +1,42 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const { Command } = require("@src/structures");
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, Message } = require("discord.js");
 
 module.exports = class CatCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: "paste",
-            description: "Paste something in sourceb.in",
-            cooldown: 5,
-            command: {
-                enabled: true,
-                category: "UTILITY",
-                botPermissions: ["EMBED_LINKS"],
-            },
-            slashCommand: {
-                enabled: false,
-            },
-        });
-    }
+  constructor(client) {
+    super(client, {
+      name: "paste",
+      description: "Paste something in sourceb.in",
+      cooldown: 5,
+      command: {
+        enabled: true,
+        category: "UTILITY",
+        botPermissions: ["EMBED_LINKS"],
+      },
+      slashCommand: {
+        enabled: false,
+      },
+    });
+  }
 
-    /**
-     * @param {Message} message
-     * @param {string[]} args
-     */
+  /**
+   * @param {Message} message
+   * @param {string[]} args
+   */
+  async messageRun(message, args) {
+    if (!args || args.length < 3) return message.channel.send("You need to add title, description and content");
 
-    async messageRun(message, args) {
+    const { key } = await fetch("https://sourceb.in/api/bins", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: args[0], description: args[1], files: [{ content: args.splice(2).join(" ") }] }),
+    }).then((res) => res.json());
 
-        if (!args || args.length < 3) return message.channel.send("You need to add title, description and content");
+    if (!key) return message.channel.send("❌ Something went wrong");
 
-
-        const { key } = await fetch("https://sourceb.in/api/bins", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ "title": args[0], "description": args[1], "files": [{ "content": args.splice(2).join(' ') }] })
-        }).then(res => res.json());
-
-
-        if (!key) return message.channel.send("❌ Something went wrong");
-
-        const embed = new MessageEmbed()
-            .setTitle("Paste links")
-            .setDescription("🔸 Normal: https://sourceb.in/" + key + "\n🔹 Raw: https://cdn.sourceb.in/bins/" + key + "/0")
-        message.channel.send({ embeds: [embed] });
-    }
+    const embed = new MessageEmbed()
+      .setTitle("Paste links")
+      .setDescription("🔸 Normal: https://sourceb.in/" + key + "\n🔹 Raw: https://cdn.sourceb.in/bins/" + key + "/0");
+    message.channel.send({ embeds: [embed] });
+  }
 };
