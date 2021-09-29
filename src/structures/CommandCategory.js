3963ee0959a00061fc2802089149e58bec4caea1
Sai Teja Madha <42540377+saiteja-madha@users.noreply.github.com>
2021-09-29 13:50:36 +0530

    help menu with buttons

---
+++ src/structures/CommandCategory.js
@@ -0,0 +1,62 @@
+module.exports = {
+  ADMIN: {
+    name: "Admin",
+    image: "https://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Settings-icon.png",
+    emoji: "\u2699",
+  },
+  AUTOMOD: {
+    name: "Automod",
+    image: "https://cdn3.iconfinder.com/data/icons/web-marketing-1-3/48/30-512.png",
+    emoji: "\uD83D\uDEE1",
+  },
+  ECONOMY: {
+    name: "Economy",
+    image: "https://icons.iconarchive.com/icons/custom-icon-design/pretty-office-11/128/coins-icon.png",
+    emoji: "\uD83E\uDE99",
+  },
+  FUN: {
+    name: "Fun",
+    image: "https://icons.iconarchive.com/icons/flameia/aqua-smiles/128/make-fun-icon.png",
+    emoji: "\uD83D\uDE02",
+  },
+  IMAGE: {
+    name: "Image",
+    image: "https://icons.iconarchive.com/icons/dapino/summer-holiday/128/photo-icon.png",
+    emoji: "\uD83D\uDDBC",
+  },
+  INVITE: {
+    name: "Invite",
+    image: "https://cdn4.iconfinder.com/data/icons/general-business/150/Invite-512.png",
+    emoji: "\uD83D\uDCE8",
+  },
+  INFORMATION: {
+    name: "Information",
+    image: "https://icons.iconarchive.com/icons/graphicloads/100-flat/128/information-icon.png",
+    emoji: "\uD83E\uDEA7",
+  },
+  MODERATION: {
+    name: "Moderation",
+    image: "https://icons.iconarchive.com/icons/lawyerwordpress/law/128/Gavel-Law-icon.png",
+    emoji: "\uD83D\uDD28",
+  },
+  MUSIC: {
+    name: "Music",
+    image: "https://icons.iconarchive.com/icons/wwalczyszyn/iwindows/256/Music-Library-icon.png",
+    emoji: "🎵",
+  },
+  SOCIAL: {
+    name: "Social",
+    image: "https://icons.iconarchive.com/icons/dryicons/aesthetica-2/128/community-users-icon.png",
+    emoji: "\uD83E\uDEC2",
+  },
+  TICKET: {
+    name: "Ticket",
+    image: "https://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/512/ticket-icon.png",
+    emoji: "🎫",
+  },
+  UTILITY: {
+    name: "Utility",
+    image: "https://icons.iconarchive.com/icons/blackvariant/button-ui-system-folders-alt/128/Utilities-icon.png",
+    emoji: "\uD83D\uDEE0",
+  },
+};
