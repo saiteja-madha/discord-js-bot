4510fbc26562027bbe900870a6264e8ad28551b2
Sai Teja Madha <42540377+saiteja-madha@users.noreply.github.com>
2021-11-15 18:26:01 +0530

    dashboard fixes

--- bot.js
+++ bot.js
@@ -4,13 +4,13 @@ require("module-alias/register");
 const path = require("path");
 const { startupCheck } = require("@utils/botUtils");
 const { BotClient } = require("@src/structures");
-const { launch } = require("@root/dashboard/app");
 
 global.__appRoot = path.resolve(__dirname);
 
 // initialize client
 const client = new BotClient();
 client.loadCommands("src/commands");
+client.loadContexts("src/contexts");
 client.loadEvents("src/events");
 
 // catch client errors and warnings
@@ -25,6 +25,7 @@ process.on("unhandledRejection", (err) => client.logger.error(`Unhandled excepti
   if (client.config.DASHBOARD.enabled) {
     client.logger.log("Launching dashboard");
     try {
+      const { launch } = require("@root/dashboard/app");
       await launch(client);
     } catch (ex) {
       client.logger.error("Failed to launch dashboard", ex);
