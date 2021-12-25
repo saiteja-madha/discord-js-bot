3e7b408eb4a9cddc26c9ec5f9b8384a6cbd4208c
Patatounet <87875788+Patatounet@users.noreply.github.com>
2021-12-26 03:01:02 +0400

    Update react.js

--- src/commands/anime/react.js
+++ src/commands/anime/react.js
@@ -3,9 +3,11 @@ const { CommandInteraction, MessageEmbed, Message } = require("discord.js");
 const { getJson } = require("@utils/httpUtils");
 const { EMBED_COLORS } = require("@root/config");
 const NekosLife = require("nekos.life");
-const neko = new NekosLife();
+
+const urlb = "https://cdn.nekos.life/"; //is first part of url
 
 const choices = ["hug", "kiss", "cuddle", "pat", "poke", "slap", "smug", "tickle", "wink"];
+const nchoices = [90, 144, 46, 75, 24, 17, 60, 21]; //array with number of gif per catégory
 
 module.exports = class Reaction extends Command {
   constructor(client) {
@@ -72,10 +74,18 @@ const genReaction = async (category, user) => {
 
     // neko api
     else {
-      imageUrl = (await neko.sfw[category]()).url;
+      find = choices.indexOf(category); //determines the location of the choice in the choices table
+      select = nchoices[find]; //select the max number of gifs thanks to find
+      let complémant = Math.floor(Math.random() * select ) + 1; //random select number with the max of "select"
+
+      if (complémant < 10) { complémant = "00" + complémant;} //add 00 if the number random is lower to 10
+      else if (complémant >= 10 && complémant < 100) { complémant = "0" + complémant; }//add 0 if the number random is higher or egal to 10 and lower 100
+      
+      imageUrl = urlb + [category] + "/" + [category] + "_" + complémant + ".gif"; //set imageUrl to fisrt part of url + choice of category + / + choice + _ + random number + .gif
+    
     }
 
-    return new MessageEmbed().setImage(imageUrl).setColor("RANDOM").setFooter(`Requested By ${user.tag}`);
+    return new MessageEmbed().setFooter(`Requested By ${user.tag}`).setImage(imageUrl);
   } catch (ex) {
     return new MessageEmbed()
       .setColor(EMBED_COLORS.ERROR)
