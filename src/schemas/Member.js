e40db68c82d996ed094ba047e3b3e45afc20331a
Sai Teja Madha <42540377+saiteja-madha@users.noreply.github.com>
2022-02-12 21:15:14 +0530

    invite lb - null fix

--- src/schemas/Member.js
+++ src/schemas/Member.js
@@ -82,7 +82,7 @@ module.exports = {
           },
         },
       },
-      { $match: { invites: { $ne: 0 } } },
+      { $match: { invites: { $gt: 0 } } },
       { $sort: { invites: -1 } },
       { $limit: limit },
     ]),
