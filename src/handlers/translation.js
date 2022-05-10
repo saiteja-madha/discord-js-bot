501198a9a8d9f9cbba1f8b767f5fd9c4611bb580
Sai Teja Madha <42540377+saiteja-madha@users.noreply.github.com>
2022-05-11 01:39:34 +0530

    country-emoji-languages integration
    https://www.npmjs.com/package/country-emoji-languages

--- src/handlers/translation.js
+++ src/handlers/translation.js
@@ -1,7 +1,7 @@
 const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 const { isTranslated, logTranslation } = require("@schemas/TranslateLog");
 const data = require("@src/data.json");
-const { getCountryLanguages } = require("country-language");
+const { getLanguagesFromEmoji } = require("country-emoji-languages");
 const { sendMessage } = require("@utils/botUtils");
 const { translate } = require("@utils/httpUtils");
 const { timeformat } = require("@utils/miscUtils");
@@ -25,70 +25,65 @@ const getTranslationCooldown = (user) => {
 };
 
 /**
- * @param {string} countryCode
+ * @param {string} emoji
  * @param {import("discord.js").Message} message
  * @param {import("discord.js").User} user
  */
-async function handleFlagReaction(countryCode, message, user) {
+async function handleFlagReaction(emoji, message, user) {
   // cooldown check
   const remaining = getTranslationCooldown(user);
   if (remaining > 0) {
     return sendMessage(message.channel, `${user} You must wait ${timeformat(remaining)} before translating again!`, 5);
   }
 
-  if (await isTranslated(message, countryCode)) return;
+  if (await isTranslated(message, emoji)) return;
 
-  getCountryLanguages(countryCode, async (err, languages) => {
-    if (err) return;
+  const languages = getLanguagesFromEmoji(emoji);
 
-    // filter languages for which google translation is available
-    const targetCodes = languages
-      .filter((language) => data.GOOGLE_TRANSLATE[language.iso639_1] !== undefined)
-      .map((language) => language.iso639_1);
+  // filter languages for which google translation is available
+  const targetCodes = languages.filter((language) => data.GOOGLE_TRANSLATE[language] !== undefined);
+  if (targetCodes.length === 0) return;
 
-    if (targetCodes.length === 0) return;
+  // remove english if there are other language codes
+  if (targetCodes.length > 1 && targetCodes.includes("en")) {
+    targetCodes.splice(targetCodes.indexOf("en"), 1);
+  }
 
-    // remove english if there are other language codes
-    if (targetCodes.length > 1 && targetCodes.includes("en")) {
-      targetCodes.splice(targetCodes.indexOf("en"), 1);
-    }
+  let src;
+  let desc = "";
+  let translated = 0;
+  for (const tc of targetCodes) {
+    const response = await translate(message.content, tc);
+    if (!response) continue;
+    src = response.inputLang;
+    desc += `**${response.outputLang}:**\n${response.output}\n\n`;
+    translated += 1;
+  }
 
-    let src;
-    let desc = "";
-    let translated = 0;
-    for (const tc of targetCodes) {
-      const response = await translate(message.content, tc);
-      if (!response) continue;
-      src = response.inputLang;
-      desc += `**${response.outputLang}:**\n${response.output}\n\n`;
-      translated += 1;
-    }
+  if (translated === 0) return;
 
-    if (translated === 0) return;
+  const btnRow = new MessageActionRow().addComponents(
+    new MessageButton({
+      url: message.url,
+      label: "Original Message",
+      style: "LINK",
+    })
+  );
 
-    const btnRow = new MessageActionRow().addComponents(
-      new MessageButton({
-        url: message.url,
-        label: "Original Message",
-        style: "LINK",
-      })
-    );
+  const embed = new MessageEmbed()
+    .setColor(message.client.config.EMBED_COLORS.BOT_EMBED)
+    .setAuthor({ name: `Translation from ${src}` })
+    .setDescription(desc)
+    .setFooter({
+      text: `Requested by ${user.tag}`,
+      iconURL: user.displayAvatarURL(),
+    });
 
-    const embed = new MessageEmbed()
-      .setColor(message.client.config.EMBED_COLORS.BOT_EMBED)
-      .setAuthor({ name: `Translation from ${src}` })
-      .setDescription(desc)
-      .setFooter({
-        text: `Requested by ${user.tag}`,
-        iconURL: user.displayAvatarURL(),
-      });
+  sendMessage(message.channel, { embeds: [embed], components: [btnRow] }).then(
+    () => cooldownCache.set(user.id, Date.now()) // set cooldown
+  );
 
-    sendMessage(message.channel, { embeds: [embed], components: [btnRow] }).then(
-      () => cooldownCache.set(user.id, Date.now()) // set cooldown
-    );
-
-    logTranslation(message, countryCode);
-  });
+  logTranslation(message, emoji);
 }
 
 module.exports = {
