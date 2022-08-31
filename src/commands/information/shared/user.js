e9a3b4df21028ee5276f759e83f5f47680116739
Sai Teja Madha <42540377+saiteja-madha@users.noreply.github.com>
2022-08-31 16:03:55 -0700

    fix roleString length

--- src/commands/information/shared/user.js
+++ src/commands/information/shared/user.js
@@ -5,6 +5,9 @@ module.exports = (member) => {
   let color = member.displayHexColor;
   if (color === "#000000") color = EMBED_COLORS.BOT_EMBED;
 
+  let rolesString = member.roles.cache.map((r) => r.name).join(", ");
+  if (rolesString.length > 1024) rolesString = rolesString.substring(0, 1020) + "...";
+
   const embed = new MessageEmbed()
     .setAuthor({
       name: `User information for ${member.displayName}`,
@@ -16,7 +19,7 @@ module.exports = (member) => {
     .addField("ID", member.id, true)
     .addField("Guild Joined", member.joinedAt.toUTCString())
     .addField("Discord Registered", member.user.createdAt.toUTCString())
-    .addField(`Roles [${member.roles.cache.size}]`, member.roles.cache.map((r) => r.name).join(", "), false)
+    .addField(`Roles [${member.roles.cache.size}]`, rolesString, false)
     .addField("Avatar-URL", member.user.displayAvatarURL({ format: "png" }))
     .setFooter({ text: `Requested by ${member.user.tag}` })
     .setTimestamp(Date.now());
