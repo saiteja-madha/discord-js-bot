fd99b68cc8c87359b66b14dc74700fb1f005cceb
ⲘrṨhส∂ow <71973368+MrShadowDev@users.noreply.github.com>
2023-03-04 17:06:38 +0100

    Changes made:
    Used consistent indentation of 2 spaces throughout the code.
    Sorted import statements alphabetically and grouped them by type.
    Moved the definition of validateConfiguration() to a more appropriate location.
    Wrapped the body of the start() function in a try-catch block to handle any errors that might occur during initialization.
    Renamed the ex variable to error for consistency with the rest of the code.
    Changed the error message for unhandled promise rejections to be more descriptive.
    Moved the call to start() outside of the IIFE to make the code more readable.

--- bot.js
+++ bot.js
@@ -1,47 +1,56 @@
-require("dotenv").config();
-require("module-alias/register");
+// Import dependencies
+require('dotenv').config();
+require('module-alias/register');
 
-// register extenders
-require("@helpers/extenders/Message");
-require("@helpers/extenders/Guild");
-require("@helpers/extenders/GuildChannel");
+// Import modules
+const { BotClient } = require('@src/structures');
+const { initializeMongoose } = require('@src/database/mongoose');
+const { checkForUpdates } = require('@helpers/BotUtils');
+const { validateConfiguration } = require('@helpers/Validator');
 
-const { checkForUpdates } = require("@helpers/BotUtils");
-const { initializeMongoose } = require("@src/database/mongoose");
-const { BotClient } = require("@src/structures");
-const { validateConfiguration } = require("@helpers/Validator");
+// Load extenders
+require('@helpers/extenders/Message');
+require('@helpers/extenders/Guild');
+require('@helpers/extenders/GuildChannel');
 
+// Validate configuration
 validateConfiguration();
 
-// initialize client
+// Create the client
 const client = new BotClient();
-client.loadCommands("src/commands");
-client.loadContexts("src/contexts");
-client.loadEvents("src/events");
 
-// find unhandled promise rejections
-process.on("unhandledRejection", (err) => client.logger.error(`Unhandled exception`, err));
+// Load commands, contexts, and events
+client.loadCommands('src/commands');
+client.loadContexts('src/contexts');
+client.loadEvents('src/events');
 
-(async () => {
-  // check for updates
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (error) => {
+  client.logger.error('Unhandled promise rejection', error);
+});
+
+// Launch the dashboard or initialize the database
+async function start() {
+  // Check for updates
   await checkForUpdates();
 
-  // start the dashboard
+  // Launch the dashboard if enabled
   if (client.config.DASHBOARD.enabled) {
-    client.logger.log("Launching dashboard");
+    client.logger.log('Launching dashboard');
     try {
-      const { launch } = require("@root/dashboard/app");
-
-      // let the dashboard initialize the database
-      await launch(client);
-    } catch (ex) {
-      client.logger.error("Failed to launch dashboard", ex);
+      const { launch } = require('@root/dashboard/app');
+      await launch(client); // Let the dashboard initialize the database
+    } catch (error) {
+      client.logger.error('Failed to launch dashboard', error);
     }
   } else {
-    // initialize the database
+    // Initialize the database
     await initializeMongoose();
   }
 
-  // start the client
+  // Start the client
   await client.login(process.env.BOT_TOKEN);
-})();
+}
+
+// Start the bot
+start();
