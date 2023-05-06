cd2bc2d113b4fdf191ddf4937df7146398a849a2
ductridev <trihd123@gmail.com>
2023-05-06 12:09:05 +0700

    Handle exception when resume after reconnected

--- src/handlers/lavaclient.js
+++ src/handlers/lavaclient.js
@@ -30,10 +30,15 @@ module.exports = (client) => {
 
     // Because sometimes the player is disconnected and cannot resume or play again.
     node.players.forEach(async (player) => {
-      if (player.queue.tracks.length > 0) { // Only player have tracks in queue
-        if (!player.connected) player.connect(); // Not connected but have tracks in queue because node is disconnected for a long time
-        if (player.paused) player.resume(); // Or user paused the player
-        if (!player.playing) player.play(); // If connected but not playing for some reasons
+      try {
+        if (player.queue.tracks.length > 0) {
+          // Only player have tracks in queue
+          if (!player.connected) player.connect(); // Not connected but have tracks in queue because node is disconnected for a long time
+          if (player.paused) player.resume(); // Or user paused the player
+          if (!player.playing) player.play(); // If connected but not playing for some reasons
+        }
+      } catch (e) {
+        client.logger.log(player.queue.tracks.length);
       }
     });
   });
@@ -47,24 +52,28 @@ module.exports = (client) => {
     client.logger.log(`Node "${node.id}" reconnecting...`);
 
     // Try reconnecting node
-    if (node.conn.canReconnect) { // If node can reconnect
-      while (true) { // Try reconnecting again and again until connection is established or max connection attempts exceeded
-        if(node.conn.active) break; // if connection is established so exit loop
-        if (!node.conn.canReconnect) { // If cannot reconnect
+    if (node.conn.canReconnect) {
+      // If node can reconnect
+      while (true) {
+        // Try reconnecting again and again until connection is established or max connection attempts exceeded
+        if (node.conn.active) break; // if connection is established so exit loop
+        if (!node.conn.canReconnect) {
+          // If cannot reconnect
           client.logger.log(`Node "${node.id}" reconnect failed!`);
           node.conn.connect(); // We need to connect by hand because node cannot reconnect
           break;
         }
-        if (node.conn.reconnectTry == 10) { // Max connection attempts exceeded
+        if (node.conn.reconnectTry == 10) {
+          // Max connection attempts exceeded
           client.logger.log(`Node "${node.id}" reconnect try times exceed!`);
           node.conn.connect(); // We need to connect by hand because node cannot reconnect
           break;
-        }
-        else{
+        } else {
           await node.conn.reconnect(); // Try reconnect and wait for response
         }
       }
-    } else { // Else, we need to connect by hand
+    } else {
+      // Else, we need to connect by hand
       node.conn.connect();
     }
   });
@@ -117,3 +126,4 @@ module.exports = (client) => {
 
   return lavaclient;
 };
+
