326bab35c1453384a9c8d6190caa647d8a8b310b
Ghellab Abderrahmane <43763935+rhaym-tech@users.noreply.github.com>
2023-08-20 00:41:28 +0900

    Handle custom presence type

--- src/handlers/presence.js
+++ src/handlers/presence.js
@@ -11,7 +11,9 @@ function updatePresence(client) {
   }
 
   if (message.includes("{members}")) {
-    const members = client.guilds.cache.map((g) => g.memberCount).reduce((partial_sum, a) => partial_sum + a, 0);
+    const members = client.guilds.cache
+      .map((g) => g.memberCount)
+      .reduce((partial_sum, a) => partial_sum + a, 0);
     message = message.replaceAll("{members}", members);
   }
 
@@ -28,18 +30,97 @@ function updatePresence(client) {
 
       case "WATCHING":
         return ActivityType.Watching;
+      case "CUSTOM":
+        return ActivityType.Custom;
     }
   };
 
-  client.user.setPresence({
-    status: client.config.PRESENCE.STATUS,
-    activities: [
-      {
-        name: message,
-        type: getType(client.config.PRESENCE.TYPE),
-      },
-    ],
-  });
+  if(client.config.PRESENCE.TYPE === "CUSTOM") {
+    client.user.setPresence({
+      status: client.config.PRESENCE.STATUS,
+      activities: [
+        {
+          name: message,
+          state: message,
+          type: getType(client.config.PRESENCE.TYPE),
+        },
+      ],
+    });
+  } else {
+    client.user.setPresence({
+      status: client.config.PRESENCE.STATUS,
+      activities: [
+        {
+          name: message,
+          type: getType(client.config.PRESENCE.TYPE),
+        },
+      ],
+    });
+  }
+}
+
+module.exports = function handlePresence(client) {
+  updatePresence(client);
+  setInterval(() => updatePresence(client), 10 * 60 * 1000);
+};const { ActivityType } = require("discord.js");
+
+/**
+ * @param {import('@src/structures').BotClient} client
+ */
+function updatePresence(client) {
+  let message = client.config.PRESENCE.MESSAGE;
+
+  if (message.includes("{servers}")) {
+    message = message.replaceAll("{servers}", client.guilds.cache.size);
+  }
+
+  if (message.includes("{members}")) {
+    const members = client.guilds.cache
+      .map((g) => g.memberCount)
+      .reduce((partial_sum, a) => partial_sum + a, 0);
+    message = message.replaceAll("{members}", members);
+  }
+
+  const getType = (type) => {
+    switch (type) {
+      case "COMPETING":
+        return ActivityType.Competing;
+
+      case "LISTENING":
+        return ActivityType.Listening;
+
+      case "PLAYING":
+        return ActivityType.Playing;
+
+      case "WATCHING":
+        return ActivityType.Watching;
+      case "CUSTOM":
+        return ActivityType.Custom;
+    }
+  };
+
+  if(client.config.PRESENCE.TYPE === "CUSTOM") {
+    client.user.setPresence({
+      status: client.config.PRESENCE.STATUS,
+      activities: [
+        {
+          name: message,
+          state: message,
+          type: getType(client.config.PRESENCE.TYPE),
+        },
+      ],
+    });
+  } else {
+    client.user.setPresence({
+      status: client.config.PRESENCE.STATUS,
+      activities: [
+        {
+          name: message,
+          type: getType(client.config.PRESENCE.TYPE),
+        },
+      ],
+    });
+  }
 }
 
 module.exports = function handlePresence(client) {
