9cf4af2612996c742de9df2f14d88ba6007c0698
Vikshan <103361575+vixshan@users.noreply.github.com>
2023-10-10 21:05:34 +0000

    dash config

--- config.js
+++ config.js
@@ -37,8 +37,8 @@ module.exports = {
 
   DASHBOARD: {
     enabled: true, // enable or disable dashboard
-    baseURL: "http://localhost:8080", // base url
-    failureURL: "http://localhost:8080", // failure redirect url
+    baseURL: "https://mochi.vikshan.me", // base url
+    failureURL: "https://mochi.vikshan.me", // failure redirect url
     port: "8080", // port to run the bot on
   },
 
