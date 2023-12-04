cd1a64c53c3a2f704deab1ab5b145cf5bf1de19e
Ghellab Abderrahmane <43763935+rhaym-tech@users.noreply.github.com>
2023-12-04 22:31:58 +0900

    Fix dublicated code

--- src/handlers/presence.js
+++ src/handlers/presence.js
@@ -30,70 +30,7 @@ function updatePresence(client) {
 
       case "WATCHING":
         return ActivityType.Watching;
-      case "CUSTOM":
-        return ActivityType.Custom;
-    }
-  };
-
-  if(client.config.PRESENCE.TYPE === "CUSTOM") {
-    client.user.setPresence({
-      status: client.config.PRESENCE.STATUS,
-      activities: [
-        {
-          name: message,
-          state: message,
-          type: getType(client.config.PRESENCE.TYPE),
-        },
-      ],
-    });
-  } else {
-    client.user.setPresence({
-      status: client.config.PRESENCE.STATUS,
-      activities: [
-        {
-          name: message,
-          type: getType(client.config.PRESENCE.TYPE),
-        },
-      ],
-    });
-  }
-}
-
-module.exports = function handlePresence(client) {
-  updatePresence(client);
-  setInterval(() => updatePresence(client), 10 * 60 * 1000);
-};const { ActivityType } = require("discord.js");
-
-/**
- * @param {import('@src/structures').BotClient} client
- */
-function updatePresence(client) {
-  let message = client.config.PRESENCE.MESSAGE;
-
-  if (message.includes("{servers}")) {
-    message = message.replaceAll("{servers}", client.guilds.cache.size);
-  }
-
-  if (message.includes("{members}")) {
-    const members = client.guilds.cache
-      .map((g) => g.memberCount)
-      .reduce((partial_sum, a) => partial_sum + a, 0);
-    message = message.replaceAll("{members}", members);
-  }
-
-  const getType = (type) => {
-    switch (type) {
-      case "COMPETING":
-        return ActivityType.Competing;
-
-      case "LISTENING":
-        return ActivityType.Listening;
-
-      case "PLAYING":
-        return ActivityType.Playing;
-
-      case "WATCHING":
-        return ActivityType.Watching;
+        
       case "CUSTOM":
         return ActivityType.Custom;
     }
