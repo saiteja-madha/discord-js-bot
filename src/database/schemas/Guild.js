551ee377506553061750611a4065be68fbf7cd35
max <122203942+DarthMayTriX@users.noreply.github.com>
2024-02-27 09:02:56 -0600

    Update Guild.js

--- src/database/schemas/Guild.js
+++ src/database/schemas/Guild.js
@@ -1,489 +1,153 @@
-const {
-  EmbedBuilder,
-  ActionRowBuilder,
-  ButtonBuilder,
-  ModalBuilder,
-  TextInputBuilder,
-  ApplicationCommandOptionType,
-  ChannelType,
-  ButtonStyle,
-  TextInputStyle,
-  ComponentType,
-} = require("discord.js");
-const { EMBED_COLORS } = require("@root/config.js");
-const { isTicketChannel, closeTicket, closeAllTickets } = require("@handlers/ticket");
-/**
- * @type {import("@structures/Command")}
- */
+const mongoose = require("mongoose");
+const { CACHE_SIZE, PREFIX_COMMANDS, STATS } = require("@root/config.js");
+const FixedSizeMap = require("fixedsize-map");
+const { getUser } = require("./User");
+
+const cache = new FixedSizeMap(CACHE_SIZE.GUILDS);
+
+const Schema = new mongoose.Schema({
+  _id: String,
+  data: {
+    name: String,
+    region: String,
+    owner: { type: String, ref: "users" },
+    joinedAt: Date,
+    leftAt: Date,
+    bots: { type: Number, default: 0 },
+  },
+  prefix: { type: String, default: PREFIX_COMMANDS.DEFAULT_PREFIX },
+  stats: {
+    enabled: Boolean,
+    xp: {
+      message: { type: String, default: STATS.DEFAULT_LVL_UP_MSG },
+      channel: String,
+    },
+  },
+  ticket: {
+    log_channel: String,
+    limit: { type: Number, default: 10 },
+    categories: [
+      {
+        _id: false,
+        name: String,
+        staff_roles: [String],
+      },
+    ],
+  },
+  automod: {
+    debug: Boolean,
+    strikes: { type: Number, default: 10 },
+    action: { type: String, default: "TIMEOUT" },
+    wh_channels: [String],
+    anti_attachments: Boolean,
+    anti_invites: Boolean,
+    anti_links: Boolean,
+    anti_spam: Boolean,
+    anti_ghostping: Boolean,
+    anti_massmention: Number,
+    max_lines: Number,
+  },
+  invite: {
+    tracking: Boolean,
+    ranks: [
+      {
+        invites: { type: Number, required: true },
+        _id: { type: String, required: true },
+      },
+    ],
+  },
+  flag_translation: {
+    enabled: Boolean,
+  },
+  modlog_channel: String,
+  max_warn: {
+    action: {
+      type: String,
+      enum: ["TIMEOUT", "KICK", "BAN"],
+      default: "KICK",
+    },
+    limit: { type: Number, default: 5 },
+  },
+  counters: [
+    {
+      _id: false,
+      counter_type: String,
+      name: String,
+      channel_id: String,
+    },
+  ],
+  welcome: {
+    enabled: Boolean,
+    channel: String,
+    content: String,
+    embed: {
+      description: String,
+      color: String,
+      thumbnail: Boolean,
+      footer: String,
+      image: String,
+    },
+  },
+  farewell: {
+    enabled: Boolean,
+    channel: String,
+    content: String,
+    embed: {
+      description: String,
+      color: String,
+      thumbnail: Boolean,
+      footer: String,
+      image: String,
+    },
+  },
+  autorole: String,
+  suggestions: {
+    enabled: Boolean,
+    channel_id: String,
+    approved_channel: String,
+    rejected_channel: String,
+    staff_roles: [String],
+  },
+});
+
+const Model = mongoose.model("guild", Schema);
+
 module.exports = {
-  name: "ticket",
-  description: "various ticketing commands",
-  category: "TICKET",
-  userPermissions: ["ManageGuild"],
-  command: {
-    enabled: true,
-    minArgsCount: 1,
-    subcommands: [
-      {
-        trigger: "setup <#channel>",
-        description: "start an interactive ticket setup",
-      },
-      {
-        trigger: "log <#channel>",
-        description: "setup log channel for tickets",
-      },
-      {
-        trigger: "limit <number>",
-        description: "set maximum number of concurrent open tickets",
-      },
-      {
-        trigger: "close",
-        description: "close the ticket",
-      },
-      {
-        trigger: "closeall",
-        description: "close all open tickets",
-      },
-      {
-        trigger: "add <userId|roleId>",
-        description: "add user/role to the ticket",
-      },
-      {
-        trigger: "remove <userId|roleId>",
-        description: "remove user/role from the ticket",
-      },
-      {
-        trigger: "category <categoryId>",
-        description: "set the ticket category channel by ID",
-      },
-    ],
-  },
-  slashCommand: {
-    enabled: true,
-    options: [
-      {
-        name: "setup",
-        description: "setup a new ticket message",
-        type: ApplicationCommandOptionType.Subcommand,
-        options: [
-          {
-            name: "channel",
-            description: "the channel where ticket creation message must be sent",
-            type: ApplicationCommandOptionType.Channel,
-            channelTypes: [ChannelType.GuildText],
-            required: true,
-          },
-        ],
-      },
-      {
-        name: "log",
-        description: "setup log channel for tickets",
-        type: ApplicationCommandOptionType.Subcommand,
-        options: [
-          {
-            name: "channel",
-            description: "channel where ticket logs must be sent",
-            type: ApplicationCommandOptionType.Channel,
-            channelTypes: [ChannelType.GuildText],
-            required: true,
-          },
-        ],
-      },
-      {
-        name: "limit",
-        description: "set maximum number of concurrent open tickets",
-        type: ApplicationCommandOptionType.Subcommand,
-        options: [
-          {
-            name: "amount",
-            description: "max number of tickets",
-            type: ApplicationCommandOptionType.Integer,
-            required: true,
-          },
-        ],
-      },
-      {
-        name: "close",
-        description: "closes the ticket [used in ticket channel only]",
-        type: ApplicationCommandOptionType.Subcommand,
-      },
-      {
-        name: "closeall",
-        description: "closes all open tickets",
-        type: ApplicationCommandOptionType.Subcommand,
-      },
-      {
-        name: "add",
-        description: "add user to the current ticket channel [used in ticket channel only]",
-        type: ApplicationCommandOptionType.Subcommand,
-        options: [
-          {
-            name: "user_id",
-            description: "the id of the user to add",
-            type: ApplicationCommandOptionType.String,
-            required: true,
-          },
-        ],
-      },
-      {
-        name: "remove",
-        description: "remove user from the ticket channel [used in ticket channel only]",
-        type: ApplicationCommandOptionType.Subcommand,
-        options: [
-          {
-            name: "user",
-            description: "the user to remove",
-            type: ApplicationCommandOptionType.User,
-            required: true,
-          },
-        ],
-      },
-      {
-        name: "category",
-        description: "set the ticket category channel by ID",
-        type: ApplicationCommandOptionType.Subcommand,
-        options: [
-          {
-            name: "id",
-            description: "the ID of the category channel to set as the ticket category",
-            type: ApplicationCommandOptionType.String,
-            required: true,
-          },
-        ],
-      },
-      {
-        name: "remove-c",
-        description: "remove the ticket category channel",
-        type: ApplicationCommandOptionType.Subcommand,
-      },      
-    ],
-  },
+  /**
+   * @param {import('discord.js').Guild} guild
+   */
+  getSettings: async (guild) => {
+    if (!guild) throw new Error("Guild is undefined");
+    if (!guild.id) throw new Error("Guild Id is undefined");
 
-  async messageRun(message, args, data) {
-    const input = args[0].toLowerCase();
-    let response;
+    const cached = cache.get(guild.id);
+    if (cached) return cached;
 
-    // Setup
-    if (input === "setup") {
-      if (!message.guild.members.me.permissions.has("ManageChannels")) {
-        return message.safeReply("I am missing `Manage Channels` to create ticket channels");
-      }
-      const targetChannel = message.guild.findMatchingChannels(args[1])[0];
-      if (!targetChannel) {
-        return message.safeReply("I could not find channel with that name");
-      }
-      return ticketModalSetup(message, targetChannel, data.settings);
+    let guildData = await Model.findById(guild.id);
+    if (!guildData) {
+      // save owner details
+      guild
+        .fetchOwner()
+        .then(async (owner) => {
+          const userDb = await getUser(owner);
+          await userDb.save();
+        })
+        .catch((ex) => {});
+
+      // create a new guild model
+      guildData = new Model({
+        _id: guild.id,
+        data: {
+          name: guild.name,
+          region: guild.preferredLocale,
+          owner: guild.ownerId,
+          joinedAt: guild.joinedAt,
+        },
+      });
+
+      await guildData.save();
     }
-
-    // log ticket
-    else if (input === "log") {
-      if (args.length < 2) return message.safeReply("Please provide a channel where ticket logs must be sent");
-      const target = message.guild.findMatchingChannels(args[1]);
-      if (target.length === 0) return message.safeReply("Could not find any matching channel");
-      response = await setupLogChannel(target[0], data.settings);
-    }
-
-    // Set limit
-    else if (input === "limit") {
-      if (args.length < 2) return message.safeReply("Please provide a number");
-      const limit = args[1];
-      if (isNaN(limit)) return message.safeReply("Please provide a number input");
-      response = await setupLimit(limit, data.settings);
-    }
-
-    // Close ticket
-    else if (input === "close") {
-      response = await close(message, message.author);
-      if (!response) return;
-    }
-
-    // Close all tickets
-    else if (input === "closeall") {
-      let sent = await message.safeReply("Closing tickets ...");
-      response = await closeAll(message, message.author);
-      return sent.editable ? sent.edit(response) : message.channel.send(response);
-    }
-
-    // Add user to ticket
-    else if (input === "add") {
-      if (args.length < 2) return message.safeReply("Please provide a user or role to add to the ticket");
-      let inputId;
-      if (message.mentions.users.size > 0) inputId = message.mentions.users.first().id;
-      else if (message.mentions.roles.size > 0) inputId = message.mentions.roles.first().id;
-      else inputId = args[1];
-      response = await addToTicket(message, inputId);
-    }
-
-    // Remove user from ticket
-    else if (input === "remove") {
-      if (args.length < 2) return message.safeReply("Please provide a user or role to remove");
-      let inputId;
-      if (message.mentions.users.size > 0) inputId = message.mentions.users.first().id;
-      else if (message.mentions.roles.size > 0) inputId = message.mentions.roles.first().id;
-      else inputId = args[1];
-      response = await removeFromTicket(message, inputId);
-    }
-    // Set ticket category
-    else if (input === "category") {
-      // Call function to set category channel in guild settings
-      response = await setTicketCategory(message.guild, args[1], data.settings);
-    }
-    // Invalid input
-    else {
-      return message.safeReply("Incorrect command usage");
-    }
-
-    if (response) await message.safeReply(response);
-  },
-
-  async interactionRun(interaction, data) {
-    const sub = interaction.options.getSubcommand();
-    let response;
-
-    // setup
-    if (sub === "setup") {
-      const channel = interaction.options.getChannel("channel");
-
-      if (!interaction.guild.members.me.permissions.has("ManageChannels")) {
-        return interaction.followUp("I am missing `Manage Channels` to create ticket channels");
-      }
-
-      await interaction.deleteReply();
-      return ticketModalSetup(interaction, channel, data.settings);
-    }
-
-    // Log channel
-    else if (sub === "log") {
-      const channel = interaction.options.getChannel("channel");
-      response = await setupLogChannel(channel, data.settings);
-    }
-
-    // Limit
-    else if (sub === "limit") {
-      const limit = interaction.options.getInteger("amount");
-      response = await setupLimit(limit, data.settings);
-    }
-
-    // Close
-    else if (sub === "close") {
-      response = await close(interaction, interaction.user);
-    }
-
-    // Close all
-    else if (sub === "closeall") {
-      response = await closeAll(interaction, interaction.user);
-    }
-
-    // Add to ticket
-    else if (sub === "add") {
-      const inputId = interaction.options.getString("user_id");
-      response = await addToTicket(interaction, inputId);
-    }
-
-    // Remove from ticket
-    else if (sub === "remove") {
-      const user = interaction.options.getUser("user");
-      response = await removeFromTicket(interaction, user.id);
-    }
-    // Set ticket category
-    else if (sub === "category") {
-      const categoryId = interaction.options.getString("id");
-      response = await setTicketCategory(interaction.guild, categoryId, data.settings);
-    }
-    else if (sub === "remove-c") {
-      response = await removeTicketCategory(interaction.guild, data.settings);
-    }
-    if (response) await interaction.followUp(response);
+    cache.add(guild.id, guildData);
+    return guildData;
   },
 };
-async function removeTicketCategory(guild, settings) {
-  // Remove the category channel ID from guild settings
-  settings.ticket.category_channel = null;
-  await settings.save();
-
-  return "Ticket category channel removed successfully";
-}
-async function setTicketCategory(guild, categoryId, settings) {
-
-  // Find the category channel by ID
-  const categoryChannel = guild.channels.cache.find(
-    (channel) => channel.type === 4 && channel.id === categoryId
-  );
-
-  // Check if the category channel was found
-  if (!categoryChannel) {
-    return "Invalid category ID provided or it does not refer to a category channel";
-  }
-
-  // Update the category channel ID in guild settings
-  settings.ticket.category_channel = categoryId;
-  await settings.save();
-
-  return "Ticket category channel set successfully";
-}
-/**
- * @param {import('discord.js').Message} param0
- * @param {import('discord.js').GuildTextBasedChannel} targetChannel
- * @param {object} settings
- */
-async function ticketModalSetup({ guild, channel, member }, targetChannel, settings) {
-  const buttonRow = new ActionRowBuilder().addComponents(
-    new ButtonBuilder().setCustomId("ticket_btnSetup").setLabel("Setup Message").setStyle(ButtonStyle.Primary)
-  );
-
-  const sentMsg = await channel.safeSend({
-    content: "Please click the button below to setup your ticket system!",
-    components: [buttonRow],
-  });
-
-  if (!sentMsg) return;
-
-  const btnInteraction = await channel
-    .awaitMessageComponent({
-      componentType: ComponentType.Button,
-      filter: (i) => i.customId === "ticket_btnSetup" && i.member.id === member.id && i.message.id === sentMsg.id,
-      time: 20000,
-    })
-    .catch((ex) => {});
-
-  if (!btnInteraction) return sentMsg.edit({ content: "No response received, cancelling setup", components: [] });
-
-  // display modal
-  await btnInteraction.showModal(
-    new ModalBuilder({
-      customId: "ticket-modalSetup",
-      title: "Ticket Setup",
-      components: [
-        new ActionRowBuilder().addComponents(
-          new TextInputBuilder()
-            .setCustomId("title")
-            .setLabel("ticket Title")
-            .setStyle(TextInputStyle.Short)
-            .setRequired(false)
-        ),
-        new ActionRowBuilder().addComponents(
-          new TextInputBuilder()
-            .setCustomId("description")
-            .setLabel("ticket Description")
-            .setStyle(TextInputStyle.Paragraph)
-            .setRequired(false)
-        ),
-        new ActionRowBuilder().addComponents(
-          new TextInputBuilder()
-            .setCustomId("footer")
-            .setLabel("ticket Footer")
-            .setStyle(TextInputStyle.Short)
-            .setRequired(false)
-        ),
-        new ActionRowBuilder().addComponents(
-          new TextInputBuilder()
-            .setCustomId("staff")
-            .setLabel("Staff Role (ID separate with ,)")
-            .setStyle(TextInputStyle.Short)
-            .setRequired(false)
-        ),
-      ],
-    })
-  );
-
-  // receive modal input
-  const modal = await btnInteraction
-    .awaitModalSubmit({
-      time: 1 * 60 * 1000,
-      filter: (m) => m.customId === "ticket-modalSetup" && m.member.id === member.id && m.message.id === sentMsg.id,
-    })
-    .catch((ex) => {});
-
-  if (!modal) return sentMsg.edit({ content: "No response received, cancelling setup", components: [] });
-
-  await modal.reply("Setting up ticket message ...");
-  const title = modal.fields.getTextInputValue("title");
-  const description = modal.fields.getTextInputValue("description");
-  const footer = modal.fields.getTextInputValue("footer");
-  const staffRoles = modal.fields
-    .getTextInputValue("staff")
-    .split(",")
-    .filter((s) => guild.roles.cache.has(s.trim()));
-
-  // send ticket message
-  const embed = new EmbedBuilder()
-    .setColor(EMBED_COLORS.BOT_EMBED)
-    .setAuthor({ name: title || "Support Ticket" })
-    .setDescription(description || "Please use the button below to create a ticket")
-    .setFooter({ text: footer || "You can only have 1 open ticket at a time!" });
-
-  const tktBtnRow = new ActionRowBuilder().addComponents(
-    new ButtonBuilder().setLabel("Open a ticket").setCustomId("TICKET_CREATE").setStyle(ButtonStyle.Success)
-  );
-
-
-  // save configuration
-  settings.ticket.staff_roles = staffRoles;
-  await settings.save();
-
-  await targetChannel.send({ embeds: [embed], components: [tktBtnRow] });
-  await modal.deleteReply();
-  await sentMsg.edit({ content: "Done! Ticket Message Created", components: [] });
-}
-
-async function setupLogChannel(target, settings) {
-  if (!target.canSendEmbeds()) return `Oops! I do have have permission to send embed to ${target}`;
-
-  settings.ticket.log_channel = target.id;
-  await settings.save();
-
-  return `Configuration saved! Ticket logs will be sent to ${target.toString()}`;
-}
-
-async function setupLimit(limit, settings) {
-  if (Number.parseInt(limit, 10) < 5) return "Ticket limit cannot be less than 5";
-
-  settings.ticket.limit = limit;
-  await settings.save();
-
-  return `Configuration saved. You can now have a maximum of \`${limit}\` open tickets`;
-}
-
-async function close({ channel }, author) {
-  if (!isTicketChannel(channel)) return "This command can only be used in ticket channels";
-  const status = await closeTicket(channel, author, "Closed by a moderator");
-  if (status === "MISSING_PERMISSIONS") return "I do not have permission to close tickets";
-  if (status === "ERROR") return "An error occurred while closing the ticket";
-  return null;
-}
-
-async function closeAll({ guild }, user) {
-  const stats = await closeAllTickets(guild, user);
-  return `Completed! Success: \`${stats[0]}\` Failed: \`${stats[1]}\``;
-}
-
-async function addToTicket({ channel }, inputId) {
-  if (!isTicketChannel(channel)) return "This command can only be used in ticket channel";
-  if (!inputId || isNaN(inputId)) return "Oops! You need to input a valid userId/roleId";
-
-  try {
-    await channel.permissionOverwrites.create(inputId, {
-      ViewChannel: true,
-      SendMessages: true,
-    });
-
-    return "Done";
-  } catch (ex) {
-    return "Failed to add user/role. Did you provide a valid ID?";
-  }
-}
-
-async function removeFromTicket({ channel }, inputId) {
-  if (!isTicketChannel(channel)) return "This command can only be used in ticket channel";
-  if (!inputId || isNaN(inputId)) return "Oops! You need to input a valid userId/roleId";
-
-  try {
-    channel.permissionOverwrites.create(inputId, {
-      ViewChannel: false,
-      SendMessages: false,
-    });
-    return "Done";
-  } catch (ex) {
-    return "Failed to remove user/role. Did you provide a valid ID?";
-  }
-}
